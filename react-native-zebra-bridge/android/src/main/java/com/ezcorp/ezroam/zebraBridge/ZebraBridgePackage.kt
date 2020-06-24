12f08d783144e9229444eac603583014ae947815
Federico Torres <federico.torres@softvision.com>
2020-06-24 11:10:06 -0300

    removed unnecesary code

--- react-native-zebra-bridge/android/src/main/java/com/ezcorp/ezroam/zebraBridge/ZebraBridgePackage.kt
+++ react-native-zebra-bridge/android/src/main/java/com/ezcorp/ezroam/zebraBridge/ZebraBridgePackage.kt
@@ -1,6 +1,5 @@
 package com.ezcorp.ezroam.zebraBridge
 
-import android.util.Log
 import com.ezcorp.ezroam.zebraBridge.di.ApplicationSingleton
 import com.ezcorp.ezroam.zebraBridge.di.ApplicationSingleton.initializeKoin
 import com.facebook.react.ReactPackage
@@ -10,15 +9,14 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 import org.koin.core.Koin
 import org.koin.core.KoinComponent
-import java.util.*
 import org.koin.core.inject
+import java.util.*
 
 class ZebraBridgePackage : ReactPackage, KoinComponent {
 
     private val bridgeModule by inject<ZebraBridgeModule>()
 
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        Log.i("Package", "Creating native modules")
         initializeKoin(reactContext)
         return Arrays.asList<NativeModule>(bridgeModule)
     }
